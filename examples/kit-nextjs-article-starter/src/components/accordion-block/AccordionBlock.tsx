4e4bc477daed4c360a3f2cb29cfa795c651a82ab
Nipuna Abeywickrama <nipu@sitecore.net>
2025-11-25 12:56:50 +0530

    Integrate useSitecore hook in AccordionBlock component to streamline page editing state management, enhancing client-side rendering capabilities.

--- examples/kit-nextjs-article-starter/src/components/accordion-block/AccordionBlock.tsx
+++ examples/kit-nextjs-article-starter/src/components/accordion-block/AccordionBlock.tsx
@@ -1,12 +1,15 @@
+'use client';
+
 import type React from 'react';
 import type { AccordionProps } from './accordion-block.props';
 import { AccordionBlockDefault } from './AccordionBlockDefault.dev';
+import { useSitecore } from '@sitecore-content-sdk/nextjs';
 
 // Data source checks are done in the child components
 
 // Default display of the component
 export const Default: React.FC<AccordionProps> = (props) => {
-  const { page } = props || {};
+  const { page } = useSitecore();
   const isPageEditing = page?.mode?.isEditing ?? false;
 
   return <AccordionBlockDefault {...props} isPageEditing={isPageEditing} />;
