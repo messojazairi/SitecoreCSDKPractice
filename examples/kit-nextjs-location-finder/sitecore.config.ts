b18089ec0f63ff842414e68e6f95e4fcef44b598
Nipuna Abeywickrama <nipu@sitecore.net>
2026-03-20 11:40:59 +0530

    Update Sitecore configuration to include API settings for edge platform

--- examples/kit-nextjs-location-finder/sitecore.config.ts
+++ examples/kit-nextjs-location-finder/sitecore.config.ts
@@ -4,4 +4,9 @@ import { defineConfig } from '@sitecore-content-sdk/nextjs/config';
  * See the documentation for `defineConfig`:
  * https://doc.sitecore.com/xmc/en/developers/content-sdk/the-sitecore-configuration-file.html
  */
-export default defineConfig({});
+export default defineConfig({api: {
+    edge: {
+      edgeUrl: 'https://edge-platform-staging.sitecore-staging.cloud',
+      contextId: '7DOJRy6n5qwzOUnIcV3Kpe'
+    },
+  },});
