b5d65dc24fcc9535add699bc6f7942a22cf2cb1f
sc-esariupendri <236554266+sc-esariupendri@users.noreply.github.com>
2026-06-23 14:16:56 +0530

    feat: add browser environment checks to improve SSR handling across components

--- examples/kit-nextjs-product-listing/src/components/image/nextImageSrc.dev.ts
+++ examples/kit-nextjs-product-listing/src/components/image/nextImageSrc.dev.ts
@@ -1,11 +1,12 @@
 import { ImageField } from '@sitecore-content-sdk/nextjs';
 import { getImageProps } from 'next/image';
 import { ImageProps } from './image.props';
+import { isBrowser } from '@/utils/browser';
 
 const getImageUrl = (imageField: ImageField, mode: ImageProps['page']['mode']) => {
   const src = imageField?.value?.src;
 
-  if (!mode.isNormal && src?.startsWith('/')) {
+  if (!mode.isNormal && isBrowser && src?.startsWith('/')) {
     return `${window.location.protocol}//${window.location.hostname}${src}`;
   }
 
