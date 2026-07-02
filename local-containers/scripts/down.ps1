8804ce1af5b40dc47ff25348d2f3e9db28d9c7a0
Prem Kumar1 <Prem_Kumar1@epam.com>
2026-07-02 18:14:35 +0530

    update component map to include TitleAndText component

--- local-containers/scripts/down.ps1
+++ local-containers/scripts/down.ps1
@@ -4,7 +4,7 @@ $RepoRoot = Resolve-Path "$PSScriptRoot\..\.."
 Write-Host "Down containers..." -ForegroundColor Green
 try {
   Push-Location $RepoRoot\local-containers
-  docker-compose down
+  docker compose down
   Pop-Location
   if ($LASTEXITCODE -ne 0) {
     Write-Error "Container down failed, see errors above."
