5c0bdac832853f91c3c50927c75a2d8c513d5d6b
Prem Kumar1 <Prem_Kumar1@epam.com>
2026-07-02 16:06:34 +0530

    add serialization items for local setup of nextjs-skate-park collection

--- examples/kit-nextjs-skate-park/src/components/title-and-text/TitleAndText.tsx
+++ examples/kit-nextjs-skate-park/src/components/title-and-text/TitleAndText.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Field, Text } from '@sitecore-content-sdk/nextjs';
+import { Field, Text, RichText } from '@sitecore-content-sdk/nextjs';
 
 interface Fields {
   Title: Field<string>;
@@ -18,13 +18,12 @@ export const Default = (props: TitleAndTextProps): React.ReactElement => {
   return (
     <div className={`container-default component ${styles}`}>
       <div data-class-change className={containerStyles}>
-        This container must be refreshed without reloading the page.
       </div>
       <h1 className="component-content title row">
         <Text field={props.fields.Title} />
       </h1>
       <div className="component-content text row">
-        <Text field={props.fields.Text} />
+        <RichText field={props.fields.Text} />
       </div>
     </div>
   );
